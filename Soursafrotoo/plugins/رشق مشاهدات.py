c37acf7541217186e1446e236eaa4a70533557ea
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2024-02-17 19:05:06 +0200

    Update رشق مشاهدات.py

--- Soursafrotoo/plugins/رشق مشاهدات.py
+++ Soursafrotoo/plugins/رشق مشاهدات.py
@@ -13,7 +13,7 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.contacts import UnblockRequest as unblock
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 
-from SourceZe import mody
+from Soursafrotoo import mody
 
 from ..core.managers import edit_or_reply
 
